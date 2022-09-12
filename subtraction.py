39d76ed1f242192ff4e7a31b71416e5c9cd67c40
UmerJavedAbbasi <113181662+UmerJavedAbbasi@users.noreply.github.com>
2022-09-12 10:01:27 +0500

    Create subtraction.py

---
+++ subtraction.py
@@ -0,0 +1,8 @@
+def Subtraction(a , b):
+    return int(int(a)-int(b))
+ 
+
+num1=input("Enter 1st number: ")
+num2=input("Enter 2nd number: ")
+
+print("Subtraction of " , num1 , "&" , num2 , "is" , Subtraction(num1 , num2))
